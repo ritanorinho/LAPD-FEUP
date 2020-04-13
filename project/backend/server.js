cd8bfa632f450f4982be35e06fe3873e2ad33d04
susanalima <up201603634@fe.up.pt>
2020-04-13 16:59:27 +0100

    Add connection to db

--- project/backend/server.js
+++ project/backend/server.js
@@ -5,11 +5,13 @@ const app = express()
 const mongoose = require('mongoose')
 const subscribersRouter = require('./routes/subscribers')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://LAPD:LAPD2020@cluster0-gi0qi.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
+
+
+
 app.use(express.json())
-app.use('/subscribers', subscribersRouter)
 app.listen(3000, () => console.log('server started'))
 
