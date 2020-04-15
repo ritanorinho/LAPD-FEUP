601cf717212eb1b90cfcdc603e094d51535feffa
susanalima <up201603634@fe.up.pt>
2020-04-15 15:02:46 +0100

    Fix typo

--- project/backend/api/routes/genre.js
+++ project/backend/api/routes/genre.js
@@ -6,7 +6,6 @@ const controller = require('../../controllers/genre');
 
 router.get('/', controller.getAll);
 
-router.get('/:categoryId', controller.getAllFromCategory,
-  );
+router.get('/:categoryId', controller.getAllFromCategory);
 
 module.exports = router;
