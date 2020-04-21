be5edafb712d68af312cd7baeb6496bebec5d89f
Rita <ritanorinho98@hotmail.com>
2020-04-21 09:43:00 +0100

    Fix some merge conflicts

--- project/backend/controllers/user.js
+++ project/backend/controllers/user.js
@@ -1,44 +1,43 @@
 'use strict'
 
-const passport = require("passport");
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
 
 function getAll (req, res) {
- 
   User.find()
     .then(users => res.json({ users }))
     .catch(error => res.status(400).json({ error }))
 }
 
-function add(req, res) {
-  const saltRounds = 10;
+function add (req, res) {
+  const saltRounds = 10
 
   bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    User.find({ email: req.body.email }).then((user) => {
-      if (user.length !== 0) res.status(406).json({ error: "Invalid email" });
+    User.find({ email: req.body.email }).then(user => {
+      if (user.length !== 0) res.status(406).json({ error: 'Invalid email' })
       else {
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
-          password: hash,
-        });
+          password: hash
+        })
         newUser
           .save()
-          .then((item) => res.json({ item }))
-          .catch((error) => res.status(400).json({ error }));
+          .then(item => res.json({ item }))
+          .catch(error => res.status(400).json({ error }))
       }
-    });
-  });
+    })
+  })
 }
 
-function login(req, res, next) {
+function login (req, res, next) {
   return passport.authenticate(
-    "login-user",
+    'login-user',
     { session: false },
     (err, passportUser, info) => {
       if (err) {
-        return next(err);
+        return next(err)
       }
       if (passportUser) {
         const reqUser = {
@@ -47,41 +46,45 @@ function login(req, res, next) {
           email: passportUser.email,
           username: passportUser.username,
           settings: passportUser.settings,
-          photo: passportUser.photo,
-        };
-function get (req, res) {
-  User.find({ _id: req.params.id })
-  .then(user => {res.json({ user })})
-  .catch(error => {res.status(400).json({ error })})
-}
-
-        req.login(reqUser, (error) => {
+          photo: passportUser.photo
+        }
+        req.login(reqUser, error => {
           if (error) {
-            return res.send({ error });
+            return res.send({ error })
           }
           return res.json({
-            user: passportUser.toAuthJSON(),
-          });
-        });
+            user: passportUser.toAuthJSON()
+          })
+        })
       } else {
-        return res.status(400).json(info);
+        return res.status(400).json(info)
       }
     }
-  )(req, res, next);
+  )
 }
 
-function hashPassword(password) {
-  return bcrypt.hashSync(password, 10);
+function get (req, res) {
+  User.find({ _id: req.params.id })
+    .then(user => {
+      res.json({ user })
+    })
+    .catch(error => {
+      res.status(400).json({ error })
+    })
 }
 
-function logout(req, res) {
-  req.logout();
-  return res.status(200).json("Logged out");
+function hashPassword (password) {
+  return bcrypt.hashSync(password, 10)
 }
 
-function getCurrent(req, res) {
-  const { payload } = req;
-  return res.status(200).json({ payload });
+function logout (req, res) {
+  req.logout()
+  return res.status(200).json('Logged out')
+}
+
+function getCurrent (req, res) {
+  const { payload } = req
+  return res.status(200).json({ payload })
 }
 
 module.exports = {
@@ -92,4 +95,4 @@ module.exports = {
   getCurrent,
   hashPassword,
   get
-};
+}
