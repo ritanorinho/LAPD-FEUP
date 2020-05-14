3c6d4a238fe61026c1f654dfcaa57449f667b60f
susanalima <up201603634@fe.up.pt>
2020-05-14 10:11:37 +0100

    Change initial page according with login

--- project/frontend/App.js
+++ project/frontend/App.js
@@ -1,109 +1,187 @@
-import React from 'react'
-import { AppLoading } from 'expo'
-import * as Font from 'expo-font'
-import { Ionicons } from '@expo/vector-icons'
-import ProfileScreen from './app/screens/ProfileScreen'
-import EventScreen from './app/screens/EventScreen'
-import SettingsScreen from './app/screens/SettingsScreen'
-import RegisterScreen from './app/screens/RegisterScreen'
-import LoginScreen from './app/screens/LoginScreen'
-import EventsScreen from './app/screens/EventsScreen'
-import ResultScreen from './app/screens/ResultScreen'
-import EmotionEvaluateScreen from './app/screens/EmotionEvaluateScreen'
-import QuizzScreen from './app/screens/QuizzScreen'
-import { createStackNavigator } from 'react-navigation-stack'
-import { createAppContainer } from 'react-navigation'
+import React from "react";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import ProfileScreen from "./app/screens/ProfileScreen";
+import EventScreen from "./app/screens/EventScreen";
+import SettingsScreen from "./app/screens/SettingsScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import EventsScreen from "./app/screens/EventsScreen";
+import ResultScreen from "./app/screens/ResultScreen";
+import EmotionEvaluateScreen from "./app/screens/EmotionEvaluateScreen";
+import QuizzScreen from "./app/screens/QuizzScreen";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import { AsyncStorage } from "react-native";
 
-const RootStack = createAppContainer(
+const RootStackLogin = createAppContainer(
   createStackNavigator(
     {
       Profile: {
         screen: ProfileScreen,
 
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
       Event: {
         screen: EventScreen,
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
       Settings: {
         screen: SettingsScreen,
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
       Login: {
         screen: LoginScreen,
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
       Register: {
         screen: RegisterScreen,
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
       Events: {
         screen: EventsScreen,
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
       Result: {
         screen: ResultScreen,
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
       EmotionEvaluate: {
         screen: EmotionEvaluateScreen,
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
       Quizz: {
         screen: QuizzScreen,
         navigationOptions: {
-          headerShown: false
-        }
+          headerShown: false,
+        },
       },
     },
     {
-      initialRouteName: 'Login'
+      initialRouteName: "Login",
     }
   )
-)
+);
+
+
+const RootStackProfile = createAppContainer(
+  createStackNavigator(
+    {
+      Profile: {
+        screen: ProfileScreen,
+
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+      Event: {
+        screen: EventScreen,
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+      Settings: {
+        screen: SettingsScreen,
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+      Login: {
+        screen: LoginScreen,
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+      Register: {
+        screen: RegisterScreen,
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+      Events: {
+        screen: EventsScreen,
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+      Result: {
+        screen: ResultScreen,
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+      EmotionEvaluate: {
+        screen: EmotionEvaluateScreen,
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+      Quizz: {
+        screen: QuizzScreen,
+        navigationOptions: {
+          headerShown: false,
+        },
+      },
+    },
+    {
+      initialRouteName: "Profile",
+    }
+  )
+);
 
 export default class App extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      isReady: false
-    }
+      isReady: false,
+      isLoggedIn: false,
+    };
   }
   state = {
-    response: ''
-  }
+    response: "",
+  };
 
-  async componentDidMount () {
+  async componentDidMount() {
     await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font
-    })
-    this.setState({ isReady: true })
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font,
+    });
+    let token = "";
+    try {
+      token = (await AsyncStorage.getItem("token")) || "";
+    } catch (error) {
+      console.log(error.message);
+    }
+    let isLoggedIn = false;
+    if (token.length > 0) isLoggedIn = true;
+
+    this.setState({ isReady: true, isLoggedIn });
   }
 
-  render () {
-    if (!this.state.isReady) {
-      return <AppLoading />
+  render() {
+    const { isReady, isLoggedIn } = this.state;
+    if (!isReady) {
+      return <AppLoading />;
     }
-
-    return <RootStack />
+    if (isLoggedIn) return <RootStackProfile />;
+    else return <RootStackLogin />;
   }
 }
